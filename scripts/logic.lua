6ee649f3d6e55cd361b4fa00c861bad11d3f32fa
Joel Barr <joelbarr1220@gmail.com>
2023-01-26 15:28:34 -0700

    Added logic for door cost of 0 or 3

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -1,20 +1,56 @@
-function levelOpen(id)
-	if id == "0" then -- Overworld
-		return true
-	elseif id == "1" then -- Space Station 1
-		return true
-	elseif id == "2" then -- DSS Souleye
-		return true
-	elseif id == "3" then -- Space Station 2
-		return true
-	elseif id == "4" then -- Warp Zone
-		return true
-	elseif id == "5" then -- Tower
-		return true
-	elseif id == "6" then -- Lab
-		return true
-	elseif id == "7" then -- Final
-		return true
-	end
-	return true
+function trinketsUnlocked(codes)
+    for i,v in ipairs(codes) do
+        count = Tracker:ProviderCountForCode("trinket" .. v)
+        if count < 1 then
+            return false
+        end
+    end
+    
+    return true
+end
+
+function doorCost()
+    return Tracker:ProviderCountForCode("doorCost")
+end
+
+function freeDoors()
+    return doorCost() <= 0 
+end
+
+function labUnlocked()
+    if freeDoors() then
+        return true
+    end
+    return trinketsUnlocked({1,2,3})
+end
+
+function towerUnlocked()
+    if freeDoors() then
+        return true
+    end
+    return trinketsUnlocked({4,5,6})
+end
+
+function spaceStationUnlocked()
+    if freeDoors() then
+        return true
+    end
+    return trinketsUnlocked({7,8,9})
+end
+
+function warpZoneUnlocked()
+    if freeDoors() then
+        return true
+    end
+    return trinketsUnlocked({10,11,12})
+end
+
+function trophyRoom()
+    count = Tracker:ProviderCountForCode("trinket")
+    return (count >= 10)
+end
+
+function finalLevel()
+    return (labUnlocked() and towerUnlocked() and 
+            spaceStationUnlocked() and warpZoneUnlocked())
 end
