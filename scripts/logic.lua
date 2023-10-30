041039f92416e9b992a5a09adae48c2f0f9fddaf
Joel Barr <joelbarr1220@gmail.com>
2023-10-30 16:56:19 -0600

    Commenting and a bit of  cleanup in logic.lua

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -1,5 +1,7 @@
+-- returns true if the key items mapped from the given list of numbers are all unlocked
+-- eg. if codes = [1,2] then returns true if trinket 1 and trinket 2 are unlocked/active
 function TrinketsUnlocked(codes)
-    for i,v in ipairs(codes) do
+    for _,v in ipairs(codes) do
         local count = Tracker:ProviderCountForCode("trinket" .. v)
         if count < 1 then
             return false
@@ -9,14 +11,20 @@ function TrinketsUnlocked(codes)
     return true
 end
 
+-- Returns the number of keys (count) needed to open any door
+-- To get the exact numerical identifiers matched to a specific door, use GetKeys and TrinketsUnlocked
 function DoorCost()
     return Tracker:ProviderCountForCode("doorCost")
 end
 
+-- Returns true if doors are always unlocked, as in the base game
 function FreeDoors()
-    return DoorCost() <= 0 
+    return DoorCost() <= 0
 end
 
+-- Returns a list of numbers (1-12) that map to required keys for the given price index (0-4)
+-- Ranges will be dependant on DoorCost.
+-- In this case keys are shiny trinkets
 function GetKeys(index)
     local dc = DoorCost()
     local offset = (index * dc)
@@ -60,12 +68,15 @@ function WarpZoneUnlocked()
     return DestinationUnlocked(REGION_INDEX_WARP_ZONE)
 end
 
+-- Check whether the NPC trinket might be available
+-- NOTE: This also requires one of two crew members to be rescued
 function NPCTrinket()
     local count = Tracker:ProviderCountForCode("trinket")
     return (count >= 10)
 end
 
+-- One check in the final level is only available when all areas are complete
 function FinalLevel()
-    return (LabUnlocked() and TowerUnlocked() and 
+    return (LabUnlocked() and TowerUnlocked() and
             SpaceStationUnlocked() and WarpZoneUnlocked())
 end
